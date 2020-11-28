5b040eab310ca6e5a9f518e40adcb4b94398bb8b
LiYunFei <215652052@qq.com>
2020-11-28 15:54:48 +0800

    提现

--- app/Http/Controllers/Api/WithdrawalController.php
+++ app/Http/Controllers/Api/WithdrawalController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Services\Api\WithdrawalService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class WithdrawalController extends Controller
