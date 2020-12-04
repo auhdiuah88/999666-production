b483e5c3abedcb1f9a5ef1eb86b962d691604b5b
llh <956732806@qq.com>
2020-12-04 21:57:46 +0800

    111

--- app/Services/Game/GameService.php
+++ app/Services/Game/GameService.php
@@ -202,7 +202,7 @@ class GameService
     public function Settlement_Queue_Test()
     {
 
-        $this->Ssc_FourService->ssc_sd(46357,1);
+        $this->SscService->ssc(1230);
         exit;
 
     }
