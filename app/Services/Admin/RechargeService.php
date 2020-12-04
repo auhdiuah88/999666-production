8fe402595659a549a34f1c15e66c1993776a5224
zhangxin <1625437980@qq.com>
2020-12-04 18:01:24 +0800

    仪表盘修改

--- app/Services/Admin/RechargeService.php
+++ app/Services/Admin/RechargeService.php
@@ -28,6 +28,7 @@ class RechargeService extends BaseService
     public function searchRechargeLogs($data)
     {
         $data = $this->getUserIds($data, "user_id");
+        dd($data);
         $list = $this->RechargeRepository->searchChargeLogs($data, ($data["page"] - 1) * $data["limit"], $data["limit"]);
         $total = $this->RechargeRepository->countSearchChargeLogs($data);
         $this->_data = ["total" => $total, "list" => $list];
