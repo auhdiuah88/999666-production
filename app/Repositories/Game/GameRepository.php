92ab0aa81e2de69d8978adeee6920cb2fa69be99
llh <956732806@qq.com>
2020-12-04 22:25:34 +0800

    111

--- app/Repositories/Game/GameRepository.php
+++ app/Repositories/Game/GameRepository.php
@@ -119,11 +119,11 @@ class GameRepository
                     if ($j == 0) {
                         $this->start_time = $row->end_time;
                         $this->end_time = $this->start_time + 120;
-                        $this->number = $row->number+1;
+                        $this->number = $row->number+2;
                     } else {
                         $this->start_time = $this->end_time + 1;
                         $this->end_time = $this->end_time + 120;
-                        $this->number = $this->number + 1;
+                        $this->number = $this->number + 2;
                     }
                 } else {
                     if ($j == 0) {
@@ -133,7 +133,7 @@ class GameRepository
                     } else {
                         $this->start_time = $this->end_time + 1;
                         $this->end_time = $this->end_time + 120;
-                        $this->number = $this->number + 1;
+                        $this->number = $this->number + 2;
                     }
                 }
 
@@ -168,11 +168,11 @@ class GameRepository
                     if ($j == 0) {
                         $this->start_time = $row->end_time;
                         $this->end_time = $this->start_time + 180;
-                        $this->number = $row->number+1;
+                        $this->number = $row->number+3;
                     } else {
                         $this->start_time = $this->end_time + 1;
                         $this->end_time = $this->end_time + 180;
-                        $this->number = $this->number + 1;
+                        $this->number = $this->number + 3;
                     }
                 } else {
                     if ($j == 0) {
@@ -182,7 +182,7 @@ class GameRepository
                     } else {
                         $this->start_time = $this->end_time + 1;
                         $this->end_time = $this->end_time + 180;
-                        $this->number = $this->number + 1;
+                        $this->number = $this->number + 3;
                     }
                 }
 
@@ -217,11 +217,11 @@ class GameRepository
                     if ($j == 0) {
                         $this->start_time = $row->end_time;
                         $this->end_time = $this->start_time + 300;
-                        $this->number = $row->number+1;
+                        $this->number = $row->number+5;
                     } else {
                         $this->start_time = $this->end_time + 1;
                         $this->end_time = $this->end_time + 300;
-                        $this->number = $this->number + 1;
+                        $this->number = $this->number + 5;
                     }
                 } else {
                     if ($j == 0) {
@@ -231,7 +231,7 @@ class GameRepository
                     } else {
                         $this->start_time = $this->end_time + 1;
                         $this->end_time = $this->end_time + 300;
-                        $this->number = $this->number + 1;
+                        $this->number = $this->number + 5;
                     }
                 }
 
