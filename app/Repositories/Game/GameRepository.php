8cdb150e897c56e46a6b42b7e294704ec7308910
llh <956732806@qq.com>
2020-12-05 14:03:09 +0800

    111

--- app/Repositories/Game/GameRepository.php
+++ app/Repositories/Game/GameRepository.php
@@ -255,7 +255,7 @@ class GameRepository
             $bq_game = $this->Cx_Game_Play->where("game_id", $id)->where('start_time', "<", ($time + 2))->where('end_time', ">", $time)->first();
         }
         $sq_game = $this->Cx_Game_Play->where("game_id", $id)->where('number', ($bq_game->number - 1))->first();
-        $pr_lx = $this->Cx_Game_Play->selsect("number,prize_number,type")->where("game_id", $id)->where("number", "<",$bq_game->number)->orderBy('start_time', 'desc')->limit(10)->get();
+        $pr_lx = $this->Cx_Game_Play->select("number,prize_number,type")->where("game_id", $id)->where("number", "<",$bq_game->number)->orderBy('start_time', 'desc')->limit(10)->get();
         $lx_game = $this->Cx_Game_Betting->where("user_id", $user_id)->where("game_id", $id)->where('betting_time', "<",$time)->orderBy('betting_time', 'desc')->limit(4)->get();
         $user_obj =$this->Cx_User->where('id',$user_id)->first();
         unset($sq_game->game_id, $sq_game->prize_time);
