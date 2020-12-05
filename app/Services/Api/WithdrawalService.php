0bc0204ee193d75b35dcc31ba6819bbac94645cd
LiYunFei <215652052@qq.com>
2020-12-05 19:34:19 +0800

    充值和提现

--- app/Services/Api/WithdrawalService.php
+++ app/Services/Api/WithdrawalService.php
@@ -266,7 +266,6 @@ class WithdrawalService extends PayService
             $this->_msg = $strategyClass->_msg;
             return false;
         }
-        print_r($where);die;
 
         $withdrawlLog = $this->WithdrawalRepository->getWithdrawalInfoByCondition($where);
         if (!$withdrawlLog) {
