dedbbf833bd853d3810a476ac2c81ff9775252fe
zhangxin <1625437980@qq.com>
2020-12-07 12:38:44 +0800

    仪表盘修改

--- app/Models/Cx_User.php
+++ app/Models/Cx_User.php
@@ -23,7 +23,7 @@ class Cx_User extends Model
     public function getCustomerServiceIdAttribute($value)
     {
         $phone = self::where("id", $value)->select(["phone"])->first();
-        dd($phone);
+        dd($value, $phone);
         return $phone->phone;
     }
 
