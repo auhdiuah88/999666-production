0c44566b69f5344ba1114672326a404e0318eaad
silenseHK <279890363@qq.com>
2020-12-10 20:23:20 +0800

    add-even-lucky

--- app/Models/Cx_Game_Config.php
+++ app/Models/Cx_Game_Config.php
@@ -11,13 +11,21 @@ class Cx_Game_Config extends Model
     protected $table = "game_config";
 
     protected $primaryKey = "id";
-
     public $timestamps = false;
+
+    protected $nameIndia = [
+        '奇数' => 'Odd',
+        '偶数' => 'Even',
+        '幸运' => 'Lucky'
+    ];
+
     public function game_c_x()
     {
         return $this->belongsTo('App\Models\Cx_Game_Betting',"game_c_x_id");
     }
 
-
+    public function getNameIndiaAttribute($value){
+        if(!intval($value))return $this->nameIndia[$value];
+    }
 
 }
