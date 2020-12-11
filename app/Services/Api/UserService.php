65b7439a566961ecfb749b174451aaa15af278a2
silenseHK <279890363@qq.com>
2020-12-11 23:16:02 +0800

    testregister

--- app/Services/Api/UserService.php
+++ app/Services/Api/UserService.php
@@ -173,7 +173,7 @@ class UserService
         // 判断是否有代理
         if (array_key_exists("code", $data)) {
             $list = $this->UserRepository->findAgentByCode($data["code"]);
-            if($list->isEmpty()){
+            if(empty($list)){
                 $this->error_code = 414;
                 $this->error = '邀请用户不存在';
                 return false;
