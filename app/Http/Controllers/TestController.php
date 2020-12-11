cd430cbc3f70685aaaa466531a549a80ce8e1415
silenseHK <279890363@qq.com>
2020-12-11 14:45:19 +0800

    verify

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Redis;
 
 class TestController extends Controller
 {
@@ -17,5 +18,10 @@ class TestController extends Controller
         $pay_type = '222';
         $money = 500;
         return ($winpay->rechargeOrder($pay_type, $money));
+    }
+
+    public function test(){
+        $res = Redis::set("REGIST_CODE:" . '9188888888', 666666);
+        dd($res);
     }
 }
