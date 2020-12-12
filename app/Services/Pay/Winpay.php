2a5804d096dbfa2b55477f77f055031802b8b330
silenseHK <279890363@qq.com>
2020-12-12 12:27:57 +0800

    qwe

--- app/Services/Pay/Winpay.php
+++ app/Services/Pay/Winpay.php
@@ -201,12 +201,12 @@ class Winpay extends PayStrategy
         $params = $request->post();
         $sign = $params['sign'];
         unset($params['sign']);
-        if ($this->generateSign($params) <> $sign) {
-            $this->_msg = 'leap-签名错误';
-            return false;
-        }
+//        if ($this->generateSign($params) <> $sign) {
+//            $this->_msg = 'leap-签名错误';
+//            return false;
+//        }
         $where = [
-            'order_no' => $request->sh_order,
+            'order_no' => $request->orderId,
         ];
         return $where;
     }
