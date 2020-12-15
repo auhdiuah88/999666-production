611450ff595dfd5db489e361b50265f1ed09eb3c
silenseHK <279890363@qq.com>
2020-12-15 17:34:51 +0800

    MTBpay

--- app/Services/Pay/MTBpay.php
+++ app/Services/Pay/MTBpay.php
@@ -66,7 +66,7 @@ class MTBpay extends PayStrategy
 
         \Illuminate\Support\Facades\Log::channel('mytest')->info('MTB_rechargeOrder', [$params]);
 
-        $res = $this->requestService->postJsonData(self::$url . '/}/ty/orderPay' , $params);
+        $res = $this->requestService->postJsonData(self::$url . '/ty/orderPay' , $params);
         if ($res['status'] != 'SUCCESS') {
             \Illuminate\Support\Facades\Log::channel('mytest')->info('MTB_rechargeOrder_return', $res);
             $this->_msg = $res['err_msg'];
