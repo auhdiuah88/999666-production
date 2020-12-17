2608d40cd7f857d638c6d1108ea0ae88fdc16758
silenseHK <279890363@qq.com>
2020-12-17 17:54:36 +0800

    修改后台返回时间为毫秒级

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -38,10 +38,12 @@ class HomeController extends Controller
     }
 
     public function getSystemTime(){
+        list($msec, $sec) = explode(' ', microtime());
+        $msectime = (float)sprintf('%.0f', (floatval($msec) + floatval($sec)) * 1000);
         return $this->AppReturn(
             200,
             '',
-            time()
+            $msectime
         );
     }
 }
