09e9076a845be08d26a360085993f31e4e7c31ef
silenseHK <279890363@qq.com>
2020-12-18 15:47:40 +0800

    充值列表

--- app/Models/Cx_User_Recharge_Logs.php
+++ app/Models/Cx_User_Recharge_Logs.php
@@ -14,6 +14,14 @@ class Cx_User_Recharge_Logs extends Model
 
     public $timestamps = false;
 
+    public function getMoneyAttribute(){
+        if(isset($this->status) && $this->status == 2){
+            return isset($this->arrive_money) ? $this->arrive_money : $this->money;
+        }else{
+            return $this->money;
+        }
+    }
+
     public function user(){
         return $this->belongsTo('App\Models\Cx_User','user_id','id');
     }
