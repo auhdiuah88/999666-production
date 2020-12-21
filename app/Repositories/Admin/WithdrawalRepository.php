62f8c3112f7cebd67ed2c03cc478751bb917a36b
silenseHK <279890363@qq.com>
2020-12-21 16:43:24 +0800

    体现列表增加备注和是否可提现

--- app/Repositories/Admin/WithdrawalRepository.php
+++ app/Repositories/Admin/WithdrawalRepository.php
@@ -73,7 +73,7 @@ class WithdrawalRepository extends BaseRepository
     public function searchRecord($data, $offset, $limit)
     {
         return $this->whereCondition($data, $this->Cx_Withdrawal_Record->with(["user" => function ($query) {
-            $query->select(["id", "balance", "cl_withdrawal", "cl_commission", "total_recharge", "cl_betting", "cl_betting_total"]);
+            $query->select(["id", "balance", "cl_withdrawal", "cl_commission", "total_recharge", "cl_betting", "cl_betting_total", "is_withdrawal", "remarks"]);
         }, "bank"]))->orderByDesc("create_time")->offset($offset)->limit($limit)->get()->toArray();
     }
 
