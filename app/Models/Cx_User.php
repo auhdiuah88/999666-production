763785126c7874b54620171decd0b59467fa0e65
silenseHK <279890363@qq.com>
2020-12-24 14:19:42 +0800

    user 增加软删除

--- app/Models/Cx_User.php
+++ app/Models/Cx_User.php
@@ -5,9 +5,13 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cx_User extends Model
 {
+
+    use SoftDeletes;
+
     protected $guarded = [];
 
     protected $table = "users";
