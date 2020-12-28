54ecf5f1d9e50930bff91e42de383f6375ca5759
silenseHK <279890363@qq.com>
2020-12-28 17:56:45 +0800

    '修改盈利排行查询条件'

--- app/Repositories/Admin/agent/AgentStatisticalReportRepository.php
+++ app/Repositories/Admin/agent/AgentStatisticalReportRepository.php
@@ -29,8 +29,10 @@ class AgentStatisticalReportRepository
     {
         $prefix = DB::getConfig('prefix');
         $offset = ($page - 1) * $size;
+
         $list = DB::select('select sum(gb.win_money - gb.money) as cha, sum(gb.money) as total_betting_money, sum(gb.win_money) as total_win_money, sum(gb.service_charge) as total_service_charge, gb.user_id, u.phone from `'.$prefix.'game_betting` gb left join `'.$prefix.'users` u on gb.user_id = u.id '. $where .' group by user_id having cha > 0 order by cha desc limit '. $offset .','.$size);
-        $total = count(DB::select('select count(*) from `'.$prefix.'game_betting` gb '. $where.' group by user_id'));
+
+        $total = count(DB::select('select sum(gb.win_money - gb.money) as cha from `'.$prefix.'game_betting` gb '. $where.' group by user_id having cha > 0'));
         return compact('list','total');
     }
 
@@ -46,7 +48,7 @@ class AgentStatisticalReportRepository
         $prefix = DB::getConfig('prefix');
         $offset = ($page - 1) * $size;
         $list = DB::select('select sum(gb.money - gb.win_money) as cha, sum(gb.money) as total_betting_money, sum(gb.win_money) as total_win_money, sum(gb.service_charge) as total_service_charge, gb.user_id, u.phone from `'.$prefix.'game_betting` gb left join `'.$prefix.'users` u on gb.user_id = u.id '. $where .' group by user_id having cha > 0 order by cha desc limit '. $offset .','.$size);
-        $total = count(DB::select('select count(*) from `'.$prefix.'game_betting` gb '. $where.' group by user_id'));
+        $total = count(DB::select('select sum(gb.money - gb.win_money) as cha from `'.$prefix.'game_betting` gb '. $where.' group by user_id having cha > 0'));
         return compact('list','total');
     }
 
