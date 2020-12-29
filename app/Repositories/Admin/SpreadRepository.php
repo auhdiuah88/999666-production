4ae7ae9c6a0c0ac04859e6186da43d53e6cb4c8a
silenseHK <279890363@qq.com>
2020-12-29 19:10:07 +0800

    '修改总后台盈利榜查询'

--- app/Repositories/Admin/SpreadRepository.php
+++ app/Repositories/Admin/SpreadRepository.php
@@ -8,6 +8,7 @@ use App\Models\Cx_Game_Betting;
 use App\Models\Cx_User;
 use App\Models\Cx_User_Balance_Logs;
 use App\Repositories\BaseRepository;
+use Illuminate\Support\Facades\DB;
 
 class SpreadRepository extends BaseRepository
 {
@@ -25,14 +26,35 @@ class SpreadRepository extends BaseRepository
         return array_column($this->Cx_User->where("reg_source_id", "<>", 1)->get("id")->toArray(), "id");
     }
 
-    public function findUsers($timeMap, $ids)
+    public function findUsers($ids, $page, $size, $status)
     {
-        return $this->Cx_User_Balance_Logs->whereBetween("time", $timeMap)->whereIn("user_id", $ids)->select(["id", "user_id", "dq_balance"])->orderBy("time")->groupBy("user_id")->get()->map(function ($item) {
-            $user = $this->Cx_User->where("id", $item->user_id)->select(["id", "phone", "balance"])->first();
-            $user->profit_loss = $user->balance - $item->dq_balance;
-            $item->user = $user;
-            return $item;
-        })->pluck("user");
+        $where = " where gb.betting_time BETWEEN " . day_start() ." AND " . day_end() . " ";
+        $where .= " AND gb.type = 1 ";
+        if($ids){
+            $where .= " AND gb.user_id in ( " . implode(',',$ids) .")";
+        }else{
+            $where .= " AND gb.user_id in ( " . 0 .")";
+        }
+
+        $prefix = DB::getConfig('prefix');
+        $offset = ($page - 1) * $size;
+        if($status == 0){
+            $list = DB::select('select sum(gb.win_money - gb.money) as cha, sum(gb.money) as total_betting_money, sum(gb.win_money) as total_win_money, sum(gb.service_charge) as total_service_charge, gb.user_id, u.phone from `'.$prefix.'game_betting` gb left join `'.$prefix.'users` u on gb.user_id = u.id '. $where .' group by user_id having cha > 0 order by cha desc limit '. $offset .','.$size);
+
+            $total = count(DB::select('select sum(gb.win_money - gb.money) as cha from `'.$prefix.'game_betting` gb '. $where.' group by user_id having cha > 0'));
+        }else{
+            $list = DB::select('select sum(gb.money - gb.win_money) as cha, sum(gb.money) as total_betting_money, sum(gb.win_money) as total_win_money, sum(gb.service_charge) as total_service_charge, gb.user_id, u.phone from `'.$prefix.'game_betting` gb left join `'.$prefix.'users` u on gb.user_id = u.id '. $where .' group by user_id having cha > 0 order by cha desc limit '. $offset .','.$size);
+            $total = count(DB::select('select sum(gb.money - gb.win_money) as cha from `'.$prefix.'game_betting` gb '. $where.' group by user_id having cha > 0'));
+        }
+
+        return compact('list','total');
+
+//        return $this->Cx_User_Balance_Logs->whereBetween("time", $timeMap)->whereIn("user_id", $ids)->select(["id", "user_id", "dq_balance"])->orderBy("time")->groupBy("user_id")->get()->map(function ($item) {
+//            $user = $this->Cx_User->where("id", $item->user_id)->select(["id", "phone", "balance"])->first();
+//            $user->profit_loss = $user->balance - $item->dq_balance;
+//            $item->user = $user;
+//            return $item;
+//        })->pluck("user");
     }
 
     public function sumServiceCharge($user_id, $timeMap)
