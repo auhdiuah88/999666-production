5937343ca7094c0b4228df27183e7471e75df2f4
silenseHK <279890363@qq.com>
2021-01-05 13:17:39 +0800

    'ws测试'

--- swoole/ws.php
+++ swoole/ws.php
@@ -7,6 +7,30 @@ $server = new Swoole\WebSocket\Server("0.0.0.0", 9501);
 
 $server->on('handshake', function (\Swoole\Http\Request $request, \Swoole\Http\Response $response) {
     print_r($request);
+    $key = base64_encode(
+        sha1(
+            $request->header['sec-websocket-key'] . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11',
+            true
+        )
+    );
+
+    $headers = [
+        'Upgrade' => 'websocket',
+        'Connection' => 'Upgrade',
+        'Sec-WebSocket-Accept' => $key,
+        'Sec-WebSocket-Version' => '13',
+    ];
+
+    // WebSocket connection to 'ws://127.0.0.1:9502/'
+    // failed: Error during WebSocket handshake:
+    // Response must not include 'Sec-WebSocket-Protocol' header if not present in request: websocket
+    if (isset($request->header['sec-websocket-protocol'])) {
+        $headers['Sec-WebSocket-Protocol'] = $request->header['sec-websocket-protocol'];
+    }
+
+    foreach ($headers as $key => $val) {
+        $response->header($key, $val);
+    }
     $response->status(101);
     $response->end();
 });
