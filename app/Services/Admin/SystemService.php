14e6dce73024f062236f4cc8e68801553ad32cb2
silenseHK <279890363@qq.com>
2021-01-24 18:42:23 +0800

    '修改admin修改常规配置接口'

--- app/Services/Admin/SystemService.php
+++ app/Services/Admin/SystemService.php
@@ -34,15 +34,26 @@ class SystemService extends BaseService
                 'ip_switch' => 0
             ];
         }
+        $is_check_recharge = $this->SettingRepository->getSettingValueByKey(SettingDic::key('IS_CHECK_RECHARGE'),false);
+        if($is_check_recharge){
+            $data->is_check_recharge = $is_check_recharge;
+        }else{
+            $data->is_check_recharge = [
+                'is_check_recharge' => 0
+            ];
+        }
         $this->_data = $data;
     }
 
     public function editSystem($data)
     {
         $ipSwitch = $data['ipSwitch']??0;
+        $isCheckRecharge = $data['isCheckRecharge']??0;
         if(isset($data['ipSwitch']))unset($data['ipSwitch']);
+        if(isset($data['isCheckRecharge']))unset($data['isCheckRecharge']);
         ##编辑setting
         $this->SettingRepository->saveSetting(SettingDic::key('IP_SWITCH'), ['ip_switch'=>$ipSwitch]);
+        $this->SettingRepository->saveSetting(SettingDic::key('IS_CHECK_RECHARGE'), ['is_check_recharge'=>$isCheckRecharge]);
         if ($this->SystemRepository->editSystem($data)) {
             $this->_msg = "编辑成功";
         } else {
