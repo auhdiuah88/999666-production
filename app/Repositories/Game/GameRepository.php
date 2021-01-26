5422ba2200271d5f58ef5343ee19f5feb8cd017c
silenseHK <279890363@qq.com>
2021-01-26 20:29:02 +0800

    '优化投注订单查询和game_start查询'

--- app/Repositories/Game/GameRepository.php
+++ app/Repositories/Game/GameRepository.php
@@ -264,7 +264,7 @@ class GameRepository
         $pr_lx = $this->Cx_Game_Play->select("number","prize_number","type")->where("game_id", $id)->where("id", "<",$bq_game->id)->orderBy('start_time', 'desc')->limit(10)->get();
         $lx_game = $this->Cx_Game_Betting->with(array(
                 'game_play' => function ($query) {
-                    $query->select('id', 'number', 'prize_number', 'game_id')->with(['game_name_p'=>function($query){
+                    $query->select('id', 'number', 'prize_number', 'game_id', 'status')->with(['game_name_p'=>function($query){
                         $query->select('id', 'name');
                     }]);
                 },
@@ -1042,7 +1042,7 @@ class GameRepository
                     $query->select('id', 'name');
                 },
                 'game_play' => function ($query) {
-                    $query->select('id', 'number', 'prize_number', 'game_id')->with(['game_name_p'=>function($query){
+                    $query->select('id', 'number', 'prize_number', 'game_id', 'status')->with(['game_name_p'=>function($query){
                         $query->select('id', 'name');
                     }]);
                 }
