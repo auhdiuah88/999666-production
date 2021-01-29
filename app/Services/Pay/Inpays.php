ee2d6efef3a20a0c9d7c604389213ea40acd1c6a
silenseHK <279890363@qq.com>
2021-01-29 13:38:58 +0800

    '测试inpays批量提现2'

--- app/Services/Pay/Inpays.php
+++ app/Services/Pay/Inpays.php
@@ -151,10 +151,10 @@ class Inpays extends PayStrategy
             'customer_email' => $withdrawalRecord->email,
         ];
         $params['sign'] = $this->generateSign($params,2);
-        $this->_msg = json_encode($params);
-        return false;
+        DB::table('php_logs')->insert(['msg'=>json_encode($params)]);
         \Illuminate\Support\Facades\Log::channel('mytest')->info('inpays_withdrawalOrder',$params);
         $res = $this->requestService->postJsonData(self::$url . 'inpays/payout/unifiedorder', $params);
+        DB::table('php_logs')->insert(['msg'=>json_encode($params)]);
         \Illuminate\Support\Facades\Log::channel('mytest')->info('inpays_withdrawalOrder2',[$res]);
         if ($res['code'] != 0) {
             $this->_msg = $res['message'];
