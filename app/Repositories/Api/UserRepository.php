60c67c989ebf0c259b0133599c88ba40d896a1f4
silenseHK <279890363@qq.com>
2021-02-05 17:19:22 +0800

    '修改充值返利'

--- app/Repositories/Api/UserRepository.php
+++ app/Repositories/Api/UserRepository.php
@@ -135,7 +135,7 @@ class UserRepository
         if(!isset($config['status']) || !isset($config['percent']) || !isset($config['max_rebate']) || !isset($config['min_recharge']))return;
         if($config['status'] != 1)return;
         if($config['percent'] <= 0)return;
-        if($config['min_recharge'] > $rechargeLog->money)return;
+        if($config['min_recharge'] > $money)return;
         $rebate = bcmul($money, $config['percent'],2);
         if($config['max_rebate'] > 0)$rebate = min($rebate, $config['max_rebate']);
         if($rebate <= 0)return;
