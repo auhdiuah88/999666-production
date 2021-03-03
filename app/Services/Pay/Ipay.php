2b46f3ed90c9258390d65c38f02788f5edf67c9c
silenseHK <279890363@qq.com>
2021-03-03 14:52:53 +0800

    '修改ipay签名方式'

--- app/Services/Pay/Ipay.php
+++ app/Services/Pay/Ipay.php
@@ -67,7 +67,8 @@ class Ipay extends PayStrategy
             $string[] = $key . '=' . $value;
         }
         $sign = (implode('&', $string)) . $secretKey;
-        return strtolower(md5($sign));
+//        return strtolower(md5($sign));
+        return md5($sign);
     }
 
     /**
