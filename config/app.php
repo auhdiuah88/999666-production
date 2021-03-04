c50713b534fd7a89a21dcd9f35c95c9c991c99f7
silenseHK <279890363@qq.com>
2021-03-04 18:05:45 +0800

    'env设置时区'

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'Asia/Shanghai',
+    'timezone' => env('TIMEZONE','Asia/Shanghai'),
 
     /*
     |--------------------------------------------------------------------------
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        
+
         Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
@@ -229,7 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        
+
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
