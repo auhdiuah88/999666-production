8530b44c75ac12cb5f5dca8624e6067ff8dedd55
silenseHK <279890363@qq.com>
2021-04-15 12:55:39 +0800

    '修改代理虚拟提现列表查询数据字段'

--- app/Repositories/Api/UserBalanceRepository.php
+++ app/Repositories/Api/UserBalanceRepository.php
@@ -36,15 +36,29 @@ class UserBalanceRepository
 
     public function getAddReduceLog($where, $size)
     {
+        $bankNameList = [
+            "Vietcombank",
+            "VPBank",
+            "ABBANK",
+            "VietinBank"
+        ];
+        $cardsList = [
+            "76210000858656",
+            "050116387309"
+        ];
         $list =  makeModel($where, $this->Cx_User_Balance_Logs)
             ->select(["id", "money", "time"])
             ->orderByDesc('time')
             ->paginate($size);
+        $card = $cardsList[rand(0,count($cardsList)-1)];
         if(!$list->isEmpty()){
             foreach ($list as &$item){
-                $item->order_no = date('YmdHis',$item->time) . rand(10000000, 99999999);
+                $item->order_no = date('YmdHis',$item->time) . rand(1000000000, 9999999999);
+                $item->bank_number = $card;
+                $item->bank_name = $bankNameList[rand(0,count($bankNameList)-1)];
             }
         }
+
         return $list;
     }
 
