ae5fcc1374febf9da05796d535004eef333dcf58
silenseHK <279890363@qq.com>
2021-05-27 18:48:16 +0800

    '修改globalpay'

--- app/Services/Pay/GlobalPay.php
+++ app/Services/Pay/GlobalPay.php
@@ -212,10 +212,7 @@ class GlobalPay extends PayStrategy
             return false;
         }
         $native_url = $res['order_data'];
-        if(strpos($native_url,"POST;") == 0){
-            $native_url = str_replace('POST;','',$native_url);
-            $is_post = 1;
-        }
+
         $resData = [
             'out_trade_no' => $order_no,
             'pay_type' => $pay_type,
