caf2ce677b8cc0f5d7c5a0309fce88c06a5230ad
silenseHK <279890363@qq.com>
2021-05-27 17:13:35 +0800

    '修改globalpay打印'

--- app/Services/Pay/GlobalPay.php
+++ app/Services/Pay/GlobalPay.php
@@ -92,8 +92,8 @@ class GlobalPay extends PayStrategy
         \Illuminate\Support\Facades\Log::channel('mytest')->info('MTB_rechargeOrder', [$params]);
 
         $res = $this->requestService->postHttpBuildQuery(self::$url . 'ty/orderPay' , $params);
+        \Illuminate\Support\Facades\Log::channel('mytest')->info('MTB_rechargeOrder_return', [$res]);
         if ($res['status'] != 'SUCCESS') {
-            \Illuminate\Support\Facades\Log::channel('mytest')->info('MTB_rechargeOrder_return', $res);
             $this->_msg = $res['err_msg'];
             return false;
         }
