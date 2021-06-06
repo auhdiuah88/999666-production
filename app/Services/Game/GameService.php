9497358c1f9135a9af4152f41933ba845ab938ba
silenseHK <279890363@qq.com>
2021-06-06 18:21:00 +0800

    '修改返佣模式2'

--- app/Services/Game/GameService.php
+++ app/Services/Game/GameService.php
@@ -197,6 +197,7 @@ class GameService
             foreach ($relationArr as $item) {
                 $pUser = $this->UserRepository->findByIdUser($item);
                 if ($pUser->rebate_rate > $cur_rate && $pUser->reg_source_id == 0) {
+                    $cur_rate = $pUser->rebate_rate;
                     $cha_rate = bcsub($pUser->rebate_rate,$cur_rate, 2);
                     $cha_rate = bcmul($cha_rate, 0.01, 3);
                     $prize = bcmul($cha_rate, $money, 2);
