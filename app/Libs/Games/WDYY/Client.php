0df0732653ab3d4655981f271cbb645688ba47f6
silenseHK <279890363@qq.com>
2021-06-28 20:32:48 +0800

    '打印wdyy'

--- app/Libs/Games/WDYY/Client.php
+++ app/Libs/Games/WDYY/Client.php
@@ -35,6 +35,7 @@ class Client extends GameStrategy
         $res = $this->doRequest(HOST . $api, $params_string, $header);
         Log::channel('plat')->info('wdyy-launch-return',[$res]);
         $res = json_decode($res,true);
+        Log::channel('plat')->info('wdyy-launch-return',$res);
         if(!$res){
             $this->_msg = 'launch request fail';
             return false;
