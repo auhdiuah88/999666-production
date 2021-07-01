de509e4a47a5565e7def27cf552d7bb4c248dd7e
silenseHK <279890363@qq.com>
2021-07-01 22:10:19 +0800

    'upay切换正式网关'

--- app/Services/Pay/BR/UPay.php
+++ app/Services/Pay/BR/UPay.php
@@ -16,14 +16,14 @@ use Illuminate\Support\Facades\DB;
 class UPay extends PayStrategy
 {
 
-//    protected static $rechargeUrl = 'https://payapi.soon-ex.com/';
-    protected static $rechargeUrl = 'https://payapitest.soon-ex.com/';
+    protected static $rechargeUrl = 'https://payapi.soon-ex.com/';
+//    protected static $rechargeUrl = 'https://payapitest.soon-ex.com/';
 
-//    protected static $withdrawUrl = 'https://payapi.soon-ex.com/';
-    protected static $withdrawUrl = 'https://payapitest.soon-ex.com/';
+    protected static $withdrawUrl = 'https://payapi.soon-ex.com/';
+//    protected static $withdrawUrl = 'https://payapitest.soon-ex.com/';
 
-//    protected static $nativeUrl = 'http://pay.soon-ex.com/#/?orderId=';
-    protected static $nativeUrl = 'http://paytest.soon-ex.com/brazil/#/?orderId=%s&phone=%s&mail=%s';
+    protected static $nativeUrl = 'https://pay.soon-ex.com/#/?orderId=%s&phone=%s&mail=%s';
+//    protected static $nativeUrl = 'http://paytest.soon-ex.com/brazil/#/?orderId=%s&phone=%s&mail=%s';
 
     private  $recharge_callback_url = '';     // 充值回调地址
     private  $withdrawal_callback_url = '';  //  提现回调地址
