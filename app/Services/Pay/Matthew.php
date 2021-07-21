dbde01bc88fd14a2054c0542fdabc88485cca932
silenseHK <279890363@qq.com>
2021-07-21 16:41:48 +0800

    '修改matthew测试收银台链接'

--- app/Services/Pay/Matthew.php
+++ app/Services/Pay/Matthew.php
@@ -22,7 +22,7 @@ class Matthew extends PayStrategy
     protected static $withdrawUrl = 'https://payapitest.soon-ex.com/';
 
 //    protected static $nativeUrl = 'http://pay.soon-ex.com/#/?orderId=';
-    protected static $nativeUrl = 'http://payapitest.soon-ex.com/#/?orderId=';
+    protected static $nativeUrl = 'http://paytest.soon-ex.com/#/?orderId=';
 
     private  $recharge_callback_url = '';     // 充值回调地址
     private  $withdrawal_callback_url = '';  //  提现回调地址
